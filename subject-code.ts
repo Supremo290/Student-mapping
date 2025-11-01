85a43720b3a91047d5ea3a1dce21d9d41ae8a881
Robbie Fuggan <148744488+Supremo290@users.noreply.github.com>
2025-11-02 00:01:56 +0800

    Add files via upload

---
+++ subject-code.ts
@@ -0,0 +1,42 @@
+interface SubjectCode {
+    codeNo: string;
+    course: string;
+    year: string | number;
+    dept : string
+}
+export interface SubjectGroup {
+    subjectId: string;
+    subjectTitle: string;
+    codes: SubjectCode[];
+}   
+
+export interface DepartmentGroup {
+  dept: string;
+  deptCode: string;
+  loadingDepartments: string[];
+}
+
+export interface Room {
+  roomId: string;
+  roomName: string;
+  capacity: number;
+}
+
+export interface ProgramGroup {
+  program: string;
+  year: any | number;
+  subjects: {
+    set: number;
+    subjectId: string;
+    subjectTitle: string;
+    codeNo: string;
+  }[];
+}
+export interface ProgramSchedule {
+  program: string;
+  year: number;
+  dept: string;
+  subjects: { subjectId: string; subjectTitle: string; codeNo: string }[];
+  schedule: { [slot: string]: string };
+  remainingSubjects?: number;
+}
